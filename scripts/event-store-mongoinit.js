19992f2cab75b2b27908c0dbc12e97db7d7457d6
Fares Eidi <fares.eidi@gmail.com>
2024-01-27 09:57:40 +0100

    update the example

--- scripts/event-store-mongoinit.js
+++ scripts/event-store-mongoinit.js
@@ -1,1 +1,4 @@
+//connect to the MongoDB instance and
+//  run rs.initiate() within the MongoDB shell environment. 
+// docker exec -it nestjs-ddd-advanced-architeture-event-store-1 mongo < ./scripts/event-store-mongoinit.js
 rs.initiate();
